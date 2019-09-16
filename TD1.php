5236eb3ff57051fe37478bd76568002c052a954f
m18010276 <mickael.maurer@etu.univ-amu.fr>
2019-09-16 16:32:43 +0200

    correction

--- TD1.php
+++ TD1.php
@@ -1,13 +1,4 @@
 <?php
-    function start_page($title)
-    {
-        echo ' <!DOCTYPE html> <html
-    lang="fr"><head><title>' . PHP_EOL . $title . '</title></head><body>' . PHP_EOL
-        ;
-    };
-
-    start_page('First PHP');
-
     echo '<strong> Voici mon premier programme PHP </strong><br/>' . "\n";
     echo 'C\'est pas mal non ? <br/>';
     $var1 = 6;
