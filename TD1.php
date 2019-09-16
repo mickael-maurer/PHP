0fd017726f4008f141715785b687eeebf6cfd3b3
m18010276 <mickael.maurer@etu.univ-amu.fr>
2019-09-16 16:39:37 +0200

    correction

--- TD1.php
+++ TD1.php
@@ -1,16 +1,19 @@
 <?php
-    echo '<strong> Voici mon premier programme PHP </strong><br/>' . "\n";
-    echo 'C\'est pas mal non ? <br/>';
-
     $var1 = 6;
     $var2 = 1.3;
     $var3 = 'Variable 3';
+    $jour = date('l F d, Y');
+    $jour_fr = date('d/m/Y', strtotime('2020-04-01'));
+
+    echo '<strong> Voici mon premier programme PHP </strong><br/>' . "\n";
+    echo 'C\'est pas mal non ? <br/>';
 
     echo "$var1 + $var2 = ";
     echo $var1 + $var2;
     echo '<br/>';
     echo date('l F d, Y');
     echo '<br/>';
-    $jour = date('l F d, Y');
     echo $jour;
+    echo '<br/>';
+    echo $jour_fr;
 ?>
