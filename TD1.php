4b351283438dd0fa48725e36e01afdf1a361a279
m18010276 <mickael.maurer@etu.univ-amu.fr>
2019-09-16 16:11:01 +0200

    correction

--- TD1.php
+++ TD1.php
@@ -1,2 +1,10 @@
 <?php
-echo '4';
+    echo '<strong> Voici mon premier programme PHP </strong><br/>' . "\n";
+    echo 'C\'est pas mal non ? <br/>';
+    $var1 = 6;
+    $var2 = 1.3;
+    $var3 = 'Variable 3';
+    echo "$var1 + $var2";
+    echo $var1 + $var2;
+    echo $var1 + $var3;
+?>
