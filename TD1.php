14cfb650e45154d8615de65b756cf2f55fb293f9
m18010276 <mickael.maurer@etu.univ-amu.fr>
2019-09-16 16:47:18 +0200

    correction

--- TD1.php
+++ TD1.php
@@ -2,8 +2,9 @@
     $var1 = 6;
     $var2 = 1.3;
     $var3 = 'Variable 3';
-    $jour = date('l F d, Y');
-    $jour_fr = date('d/m/Y', strtotime('2001-12-03'));
+    $jour_1 = date('l F d, Y');
+    $jour_2 = date('d/m/Y', strtotime('2001-03-12'));
+    $jour_3 = date('d/m/Y', date('2001-03-12'));
 
     echo '<strong> Voici mon premier programme PHP </strong><br/>' . "\n";
     echo 'C\'est pas mal non ? <br/>';
@@ -13,7 +14,9 @@
     echo '<br/>';
     echo date('l F d, Y');
     echo '<br/>';
-    echo $jour_fr;
+    echo $jour_1;
     echo '<br/>';
-    echo $jour_fr;
+    echo $jour_2;
+    echo '<br/>';
+    echo $jour_3;
 ?>
