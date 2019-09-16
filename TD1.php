a7edc03cc48eddb1f38e432c308383c276450768
m18010276 <mickael.maurer@etu.univ-amu.fr>
2019-09-16 16:44:45 +0200

    correction

--- TD1.php
+++ TD1.php
@@ -3,7 +3,7 @@
     $var2 = 1.3;
     $var3 = 'Variable 3';
     $jour = date('l F d, Y');
-    $jour_fr = date('d/m/Y', strtotime('2020-04-01'));
+    $jour_fr = date('d/m/Y', strtotime('2001-12-03'));
 
     echo '<strong> Voici mon premier programme PHP </strong><br/>' . "\n";
     echo 'C\'est pas mal non ? <br/>';
@@ -13,7 +13,7 @@
     echo '<br/>';
     echo date('l F d, Y');
     echo '<br/>';
-    echo $jour;
+    echo $jour_fr;
     echo '<br/>';
     echo $jour_fr;
 ?>
