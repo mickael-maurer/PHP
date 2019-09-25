29d839f80863d4eeb69de6bada3b4b5000fea7f8
m18010276 <mickael.maurer@etu.univ-amu.fr>
2019-09-25 09:42:08 +0200

    correction

--- TD2.php
+++ TD2.php
@@ -3,8 +3,8 @@
     //Correspondàl’adresseIPdeceluiquivisualiselapage.
     echo getenv('REMOTE_ADDR'), '<br/>';
     //Correspondàl’adresseIPduserveur<.
-    echo getenv('HTTP_HOST')<br/>;
+    echo getenv('HTTP_HOST'), '<br/>';
     //logicielserveurWebutilisé.
-    echo getenv('SERVER_SOFTWARE')<br/>;
+    echo getenv('SERVER_SOFTWARE'), '<br/>';
     phpinfo();
 ?>
