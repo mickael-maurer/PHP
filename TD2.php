165c1e643710e3516cd89bd88eff3a934e54382f
m18010276 <mickael.maurer@etu.univ-amu.fr>
2019-09-25 10:10:30 +0200

    correction

--- TD2.php
+++ TD2.php
@@ -12,7 +12,18 @@
 ?>
 
 <form>
-
+    <input type="text"/>
+    <input type="radio"/>
+    <input type="text"/>
+    <input type="password"/>
+    <input type="password"/>
+    <input type="text"/>
+    <select>
+        <option value="France" >France</option>
+        <option value="Pakistan" >Pakistan</option>
+    </select>
+    <input type="checkbox"/>
+    <input type="submit"/>
 </form>
 
 <?php
