28323a6279061b2c4d57c7521dd8c84fee83d6d1
m18010276 <mickael.maurer@etu.univ-amu.fr>
2019-09-25 10:13:10 +0200

    correction

--- TD2.php
+++ TD2.php
@@ -1,14 +1,6 @@
 <?php
     include 'utils.inc.php';
 
-    start_page('oui');
-    //Correspondàl’adresseIPdeceluiquivisualiselapage.
-    echo getenv('REMOTE_ADDR'), '<br/>';
-    //Correspondàl’adresseIPduserveur<.
-    echo getenv('HTTP_HOST'), '<br/>';
-    //logicielserveurWebutilisé.
-    echo getenv('SERVER_SOFTWARE'), '<br/>';
-    phpinfo();
 ?>
 
 <form>
